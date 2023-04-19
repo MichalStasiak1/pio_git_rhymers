5ae7bfe1734367994f448a948c7323431693c3cb
240695 <240695@edu.p.lodz.pl>
2023-04-19 12:38:08 +0200

    Poprawiono wszystkie błędy

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
-    public static final int NEGATIVE_ONE = -1;
+    public static final int ERROR = -1;
     Node last;
     int index;
 
@@ -26,13 +26,13 @@ public class IntLinkedList {
 
     public int top() {
         if (isEmpty())
-            return NEGATIVE_ONE;
+            return ERROR;
         return last.value;
     }
 
     public int pop() {
         if (isEmpty())
-            return NEGATIVE_ONE;
+            return ERROR;
         int ret = last.value;
         last = last.prev;
         return ret;
